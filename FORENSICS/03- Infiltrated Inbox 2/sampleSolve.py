14ed190b061012e6c4f06407828703c34b38f2a2
Varun Ramesh <122943005+InZaYn26@users.noreply.github.com>
2023-09-10 16:45:13 -0400

    Add files via upload

---
+++ FORENSICS/03- Infiltrated Inbox 2/sampleSolve.py
@@ -0,0 +1,14 @@
+import datetime
+import dkim
+
+with open('C:/Users/Varun Ramesh/OneDrive/Desktop/03- Infiltrated Inbox 2/RE-ETH-Transaction-Records-Solved.eml', 'r') as file:
+    data = file.read()
+    date = datetime.datetime(2023,8,20,13,6,0)
+    for x in range(1000):
+        date += datetime.timedelta(seconds=1)
+        modified = data.replace('<placeholder>', date.strftime('%H:%M:%S'))
+        toDecode = bytes (modified,"ascii")
+        print(date.strftime('%H:%M:%S'))
+        if(dkim.verify(toDecode)):
+         print('Answer: ' + str(date))
+        break
