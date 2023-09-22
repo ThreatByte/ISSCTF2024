249e39ecfe3f12951270cb2aeaa9411e6559ada5
Varun Ramesh <122943005+InZaYn26@users.noreply.github.com>
2023-09-21 20:00:43 -0400

    Update Hijacked.cpp
    gotta change the agent names, courtesy of chatgpt

--- REVERSE ENGINEERING/00- Hijacked Base/Hijacked.cpp
+++ REVERSE ENGINEERING/00- Hijacked Base/Hijacked.cpp
@@ -8,111 +8,111 @@
 class Hijacked {
 	
 public:
-	void MissionAlpha() {
-    	Transmit("VmxkNGIxUXlVWGxTYkdoUVYwWmFZVlpxU2xOak1WWlZVbXhPVTAxc1dYcFZSbEYzVUZFOVBRPT0="); // EspionageCTF{
+	void AgentAlpha() {
+    	Mission("VmxkNGIxUXlVWGxTYkdoUVYwWmFZVlpxU2xOak1WWlZVbXhPVTAxc1dYcFZSbEYzVUZFOVBRPT0="); // EspionageCTF{
     }
 
-    void MissionBravo() {
-    	Transmit("Vm0xd1IxbFdiRmhTYmtwT1ZteHdVRll3VlRGalJsWjBUVlJTYWxKc1ZqTldNbmhoVlVaV1ZVMUVhejA9"); //inzayn
+    void AgentBravo() {
+    	Mission("Vm0xd1IxbFdiRmhTYmtwT1ZteHdVRll3VlRGalJsWjBUVlJTYWxKc1ZqTldNbmhoVlVaV1ZVMUVhejA9"); //inzayn
     } 
 
-    void MissionCharlie() {
-       Transmit("Vm0weGQxSXhiRmhTV0doVlYwZDRWRmx0Y3pGalZsWnpXa2M1V0ZKdGVIcFdWM1JyWVd4S2MxTnNiRnBOUjJoUVZrZDRkMDVyTVVWaGVqQTk="); //abcdefooo
+    void AgentCharlie() {
+       Mission("Vm0weGQxSXhiRmhTV0doVlYwZDRWRmx0Y3pGalZsWnpXa2M1V0ZKdGVIcFdWM1JyWVd4S2MxTnNiRnBOUjJoUVZrZDRkMDVyTVVWaGVqQTk="); //abcdefooo
     }
     
-	void MissionDelta() {
-        Transmit("V1hwS2IySkhSWGxXYWxaaFRXdGFNRmxXWkZaUVVUMDk="); // shekeygamie
+	void AgentDelta() {
+        Mission("V1hwS2IySkhSWGxXYWxaaFRXdGFNRmxXWkZaUVVUMDk="); // shekeygamie
     }
 
-    void MissionEcho() {
-    	Transmit("VmpGa05HRXhTWGxVYkdScFVtMTRWVmx0ZUhkalJsSlZVbTFHYTFadGVGZFhhMlF3WVVaR1ZVMUVhejA9");//majarooughga
+    void AgentEcho() {
+    	Mission("VmpGa05HRXhTWGxVYkdScFVtMTRWVmx0ZUhkalJsSlZVbTFHYTFadGVGZFhhMlF3WVVaR1ZVMUVhejA9");//majarooughga
   
     }
     
-	void MissionFoxtrot() {
-       Transmit("Vm0weGQxTnRVWGxWV0dSUFZtMW9WbFl3WkRSV1ZteFZVMjA1Vmxac2NIbFdiR2hyVlVaV1ZVMUVhejA9"); // follow*
+	void AgentFoxtrot() {
+       Mission("Vm0weGQxTnRVWGxWV0dSUFZtMW9WbFl3WkRSV1ZteFZVMjA1Vmxac2NIbFdiR2hyVlVaV1ZVMUVhejA9"); // follow*
     }
 
-    void MissionGolf() {
-        Transmit("Vm1wR2EwMUhSWGhYV0d4VVlUSm9WVmx0ZUdGWFJteFlaVVYwYWsxV1dsaFdWbWhyVlVaV1ZVMUVhejA9"); //leenuxx
+    void AgentGolf() {
+       Mission("Vm1wR2EwMUhSWGhYV0d4VVlUSm9WVmx0ZUdGWFJteFlaVVYwYWsxV1dsaFdWbWhyVlVaV1ZVMUVhejA9"); //leenuxx
     }
 
-    void MissionHotel() {
-        Transmit("V2tWa2IySkJQVDA9"); //the*
+    void AgentHotel() {
+        Mission("V2tWa2IySkJQVDA9"); //the*
     }
 
-    void MissionIndia() {
-    	Transmit("Vm1wR1UxTXlTWGROVm1oVFlteGFXRmxYZEV0amJGbDNWbFJDYkZKdFVsWlZNblJyWVd4SmVGZHFRbGRpVkZaWFZVWkZPVkJSUFQwPQ==");//chujutusucaizen
+    void AgentIndia() {
+    	Mission("Vm1wR1UxTXlTWGROVm1oVFlteGFXRmxYZEV0amJGbDNWbFJDYkZKdFVsWlZNblJyWVd4SmVGZHFRbGRpVkZaWFZVWkZPVkJSUFQwPQ==");//chujutusucaizen
         
     }
 
-    void MissionJuliet() {
-        Transmit("1VjFaV2ExSXlUWGxUV0hCaFUwWktjRlJYY0hOT1ZtUlhZVVU1YUZJd1ZqVldiWEJXVUZFOVBRPT0="); //halowmoorasakey
+    void AgentJuliet() {
+        Mission("1VjFaV2ExSXlUWGxUV0hCaFUwWktjRlJYY0hOT1ZtUlhZVVU1YUZJd1ZqVldiWEJXVUZFOVBRPT0="); //halowmoorasakey
     }
 
-    void MissionKilo() {
-          Transmit("YUhWdWJtOTVaVzVu"); //hunnoyeng
+    void AgentKilo() {
+          Mission("YUhWdWJtOTVaVzVu"); //hunnoyeng
     }
 
-    void MissionLima() {
-        Transmit("WTIwNU0yRlhUbTloV0ZKc1ltMTBNV1ZSUFQwPQ=="); //rowichitenkuy
+    void AgentLima() {
+        Mission("WTIwNU0yRlhUbTloV0ZKc1ltMTBNV1ZSUFQwPQ=="); //rowichitenkuy
     }
 
-    void MissionMike() {
-        Transmit("Vm0weGQxTXdOVWhTYmtwT1ZtMW9WVmxyWkc5WFJteHlXa2M1VmxKc2NIbFdNakZIWVcxS1IxZHVhRlppV0dneldXdGFZV1JHVm5OaFJtaG9UVzFvVVZkV1ZtdFZNRFZ5VFZWV2FHVnFRVGs9"); //enfynytevaid
+    void AgentMike() {
+        Mission("Vm0weGQxTXdOVWhTYmtwT1ZtMW9WVmxyWkc5WFJteHlXa2M1VmxKc2NIbFdNakZIWVcxS1IxZHVhRlppV0dneldXdGFZV1JHVm5OaFJtaG9UVzFvVVZkV1ZtdFZNRFZ5VFZWV2FHVnFRVGs9"); //enfynytevaid
     }
 
-    void MissionNovember() {
-        Transmit("VjJwS2MwMXNjRmhPUkRBOQ==");  //given*
+    void AgentNovember() {
+        Mission("VmpKd1MyTXdNWE5qUm1oUFVrUkJPUT09");  //given*
     }
 
-    void MissionOscar() {
-      Transmit("VjFSS2IySkhUblZUYWxaaFUwWmFNUT09"); //cherrydun 
+    void AgentOscar() {
+      Mission("VjFSS2IySkhUblZUYWxaaFUwWmFNUT09"); //cherrydun 
     }
 
-    void MissionPapa() {
-        Transmit("VjFod1MySXdOVmRpU0ZaWFltMTRZVlJVVGtOak1XUlZVMjA1VGxZd05UQlphMUpEV1ZaV1ZVMUVhejA9"); //shyburczechurite 
+    void AgentPapa() {
+        Mission("VjFod1MySXdOVmRpU0ZaWFltMTRZVlJVVGtOak1XUlZVMjA1VGxZd05UQlphMUpEV1ZaV1ZVMUVhejA9"); //shyburczechurite 
         
     }
 
-    void MissionQuebec() {
-        Transmit("Vm1wR1lWbFhTWGhWV0d4WFltczFiMVJVVGtOV01XeHpWV3RrVlUxWGVGbGFSVlpyVkRGYWMxTnViRlppUmtwNlZsWmFWMVpWTVVWaGVqQTk="); //issctf2024
+    void AgentQuebec() {
+        Mission("Vm1wR1lWbFhTWGhWV0d4WFltczFiMVJVVGtOV01XeHpWV3RrVlUxWGVGbGFSVlpyVkRGYWMxTnViRlppUmtwNlZsWmFWMVpWTVVWaGVqQTk="); //issctf2024
     }
 
-    void MissionRomeo() {
-          Transmit("V1hwS1YySkhVa2hXYlhoclZqRndkQT09"); //seeteeuff
+    void AgentRomeo() {
+          Mission("V1hwS1YySkhVa2hXYlhoclZqRndkQT09"); //seeteeuff
     }
 
-    void MissionSierra() {
-        Transmit("b3JkZXJzfQ=="); //orders}*
+    void AgentSierra() {
+        Mission("b3JkZXJzfQ=="); //orders}*
     }
 
-    void MissionTango() {
-        Transmit("VmpGa05HRXhTWGxVV0d4VVlUSjRhRlZxU205alZteHpWMnhhVUZWVU1Eaz0="); //mallware
+    void AgentTango() {
+        Mission("VmpGa05HRXhTWGxVV0d4VVlUSjRhRlZxU205alZteHpWMnhhVUZWVU1Eaz0="); //mallware
     }
 
-    void MissionUniform() {
-        Transmit("YTNKcGNIUnZaM0poWm1abA=="); //kriptograffe
+    void AgentUniform() {
+        Mission("YTNKcGNIUnZaM0poWm1abA=="); //kriptograffe
     }
 
-    void MissionVictor() {
-          Transmit("VmpKMGFtVkdXWGxUV0docFUwWmFiMVV3WkZOalJsWjFZMGhLVGxadVFrZFZiRkp6VlZaV1ZVMUVhejA9"); //vehbxployt
+    void AgentVictor() {
+         Mission("VmpKMGFtVkdXWGxUV0docFUwWmFiMVV3WkZOalJsWjFZMGhLVGxadVFrZFZiRkp6VlZaV1ZVMUVhejA9"); //vehbxployt
     }
 
-    void MissionWhiskey() {
-        Transmit("VjFSSmVGWXlTa2hWV0d4WFltMTRjVlJVU2xOa01XeDBUVmR3VVZWVU1Eaz0="); //reewerswing
+    void AgentWhiskey() {
+        Mission("VjFSSmVGWXlTa2hWV0d4WFltMTRjVlJVU2xOa01XeDBUVmR3VVZWVU1Eaz0="); //reewerswing
     }
 
-    void MissionXray() {
-        Transmit("VmpGU1EyRXlTWGxWYmxKVllteEtXRmx0ZUdGWlZsSllUVlJTYWxadGVGcFhXSEJ6VlZaV1ZVMUVhejA9"); //phorencick
+    void AgentXray() {
+        Mission("VmpGU1EyRXlTWGxWYmxKVllteEtXRmx0ZUdGWlZsSllUVlJTYWxadGVGcFhXSEJ6VlZaV1ZVMUVhejA9"); //phorencick
     }
 
-    void MissionYankee() {
-        Transmit("Vm0weGQxSXlSbkpOVm1oVlYwZDRWMWxyWkZOalJsWjBUVlJTVjFadGVEQmFSVll3WVdzeFYxWnFUbGRpV0ZGM1dXdGtTMU5IVmtsaFJscE9ZV3hhVVZkV1ZtdFZNRFZ5VFZWV2FHVnFRVGs9"); //bynarixploid
+    void AgentYankee() {
+        Mission("Vm0weGQxSXlSbkpOVm1oVlYwZDRWMWxyWkZOalJsWjBUVlJTVjFadGVEQmFSVll3WVdzeFYxWnFUbGRpV0ZGM1dXdGtTMU5IVmtsaFJscE9ZV3hhVVZkV1ZtdFZNRFZ5VFZWV2FHVnFRVGs9"); //bynarixploid
     }
 
-    void MissionZulu() {
-        Transmit("b3Blbm5zb3JzZWludA=="); //opennsorseint
+    void AgentZulu() {
+        Mission("b3Blbm5zb3JzZWludA=="); //opennsorseint
     }
 
 
@@ -121,15 +121,12 @@ public:
     }
 
 private:
-    void Transmit(const std::string& Data) {
+    void Mission(const std::string& Data) {
         if (!Briefing.empty()) {
             Briefing += "";
         }
         Briefing += Data;
     }
-
-    
-    
 
 private:
     std::string Briefing;
@@ -148,34 +145,33 @@ int main() {
     std::cout <<"\n";
     
     
-    Base.MissionAlpha();
-    Base.MissionBravo();
-    Base.MissionCharlie();
-    Base.MissionDelta();
-    Base.MissionEcho();
-    Base.MissionFoxtrot();
-    Base.MissionGolf();
-    Base.MissionHotel();
-    Base.MissionIndia();
-    Base.MissionJuliet();
-    Base.MissionKilo();
-    Base.MissionLima();
-    Base.MissionMike();
-    Base.MissionNovember();
-    Base.MissionOscar();
-    Base.MissionPapa();
-    Base.MissionQuebec();
-    Base.MissionRomeo();
-    Base.MissionSierra();
-    Base.MissionTango();
-    Base.MissionUniform();
-    Base.MissionVictor();
-    Base.MissionWhiskey();
-    Base.MissionXray();
-    Base.MissionYankee();
-    Base.MissionZulu();
+    Base.AgentAlpha();
+    Base.AgentBravo();
+    Base.AgentCharlie();
+    Base.AgentDelta();
+    Base.AgentEcho();
+    Base.AgentFoxtrot();
+    Base.AgentGolf();
+    Base.AgentHotel();
+    Base.AgentIndia();
+    Base.AgentJuliet();
+    Base.AgentKilo();
+    Base.AgentLima();
+    Base.AgentMike();
+    Base.AgentNovember();
+    Base.AgentOscar();
+    Base.AgentPapa();
+    Base.AgentQuebec();
+    Base.AgentRomeo();
+    Base.AgentSierra();
+    Base.AgentTango();
+    Base.AgentUniform();
+    Base.AgentVictor();
+    Base.AgentWhiskey();
+    Base.AgentXray();
+    Base.AgentYankee();
+    Base.AgentZulu();
     Base.Report();
 
     return 0;
 };
-
