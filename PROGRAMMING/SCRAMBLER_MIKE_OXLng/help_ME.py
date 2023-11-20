9287870286f2e097d3bff8dcbf1bdec07a9ef4a0
Michael A Nolk <74033735+nolkm@users.noreply.github.com>
2023-11-19 21:05:16 -0500

    Update help_ME.py

--- PROGRAMMING/SCRAMBLER_MIKE_OXLng/help_ME.py
+++ PROGRAMMING/SCRAMBLER_MIKE_OXLng/help_ME.py
@@ -1,30 +1,23 @@
-import base64
 import random
 import string
 
-def my_encode(s):
-    return base64.b64encode(s.encode()).decode()
 
-def my_decode(s):
-    return base64.b64decode(s.encode()).decode()
-
-def my_scramble(s):
+def SCRAMBLER(s):
     return ''.join(random.sample(s, len(s)))
 
-def my_unscramble(s):# ¯\_(ツ)_/¯  implement this function
+def UNSCRAMBLER(s):# ¯\_(ツ)_/¯  implement this function
     # Build This Func
 
     pass
 
 def generate_my_secret(): 
-    my_alphabet = string.ascii_letters + string.digits
-    my_key = my_scramble(my_alphabet)
-    my_message = "My secret code is: " + my_key
-    my_encrypted = my_encode(my_message)
-    return my_encrypted
+    alphabet = string.ascii_letters + string.digits
+    key = SCRAMBLER(alphabet)
+    message = "My secret code is: " + key
+    encrypted = SCRAMBLER(message)
+    return encrypted
 
-my_secret = generate_my_secret()
-print(my_secret)
+hidden = generate_my_secret()
 
-#original_key = my_unscramble(my_secret)
-print("Original Key:", original_key)
+#original_key = UNSCRAMBLER(hidden) #uncomment after 
+print("Original Key:", hidden)
