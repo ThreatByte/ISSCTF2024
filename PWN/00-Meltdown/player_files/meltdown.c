1e3d2278346d49e24d51ac9da4976944f87523bf
boylanja <98715540+boylanja@users.noreply.github.com>
2023-11-26 14:44:10 -0500

    Update meltdown.c

--- PWN/00-Meltdown/player_files/meltdown.c
+++ PWN/00-Meltdown/player_files/meltdown.c
@@ -2,44 +2,29 @@
 #include <stdlib.h>
 
 int main(){
+	printf("Welcome to the reactor's admin interface!\n");
 	int done = 0;
 	int fTemp = 70;
 	while(done == 0){
 		int choice;
 		int cTemp;
-		printf("Welcome to the reactor's admin interface!\n'");
-		printf("What would you like to do?\n");
-		
-		printf("1. Check temperature\n");
-		printf("2. Change temperature\n");
-		printf("3. Exit\n");
+		printf("What temperature would you like to set the reactor to? (Celsius)\n");
 		fflush(stdin);
-		scanf("%d", &choice);	
-		if(choice == 1){
+		scanf("%d", &cTemp);
+		if(cTemp < 500){
+			//whose idea was it to take celsius input and then convert to fahrenheit??????????
+			fTemp = cTemp * 2 + 32;
 			if(fTemp > 2000000000){
+				printf("temperature is %d degress F\n\n", fTemp);
 				printf("AAAAAAAAAAAAAAAAAAAAH TOO HOT!!!!!!\n");
 				printf("FLAG\n");
+				done = 1;
 			}else{
 				printf("temperature is %d degress F\n\n", fTemp);
 			}
-		
-		
-		}else if(choice == 2){
-			printf("What temperature would you like to set the reactor too? (Celsius)\n");
-			fflush(stdin);
-			scanf("%d", &cTemp);
-			if(cTemp < 500){
-				//whose idea was it to take celsius input and then convert to fahrenheit
-				fTemp = cTemp * 2 + 32;
-				printf("temperature changed to: %d degrees F\n\n", fTemp);
 				
 			}else{
 				printf("Woah! Too hot, could not change\n\n");
 			}
-		
-		}else{
-			done = 1;
-		}
-		
 	}
 }
