a61c972b51794f7983d954f535ccd363c3111d56
Varun Ramesh <122943005+InZaYn26@users.noreply.github.com>
2023-11-27 20:42:44 -0500

    Update solve.py

--- FORENSICS/03-Infiltrated Inbox 2/solve.py
+++ FORENSICS/03-Infiltrated Inbox 2/solve.py
@@ -1,1 +1,15 @@
+import datetime
+import dkim
+
+with open('C:/your/path', 'r') as file:
+    data = file.read()
+    date = datetime.datetime(2023,8,20,13,6,0)
+    for x in range(1000):
+        date += datetime.timedelta(seconds=1)
+        modified = data.replace('<placeholder>', date.strftime('%H:%M:%S'))
+        toDecode = bytes (modified,"ascii")
+        print(date.strftime('%H:%M:%S'))
+        if(dkim.verify(toDecode)):
+         print('Answer: ' + str(date))
+        break
 
